5041c76532ada911fe9f3dffb61d7b77f08dbe30
Pri_030 <priyankasingh30@gmail.com>
2020-05-01 13:45:51 -0400

    updated javascript file

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,26 +1,20 @@
-var paraEl = document.querySelector("#currentDay");
-var paraE2 = document.querySelector("#cal");
-
 //get the current data and day
-var dateEl = moment().format("MMMM Do YYYY, dddd");
-//var date1El = moment().calendar();
-//var date1El = moment().fromNow("hhh");
-// var date1El = moment().hours(10);
-// var tt = moment().get('hour');
-// var ttr = moment().set('hour', 13);
-var currentTime = moment();
-console.log(currentTime);
-currentTime = currentTime.startOf("day");
+var currentDate = moment().format("MMMM Do YYYY, dddd"); //moment() will give the current date and time format the date using format()
+$("#currentDay").text(currentDate);
 
-var hourEl = currentTime.add(9, 'h');
-var rr = moment().hour("13").minutes("00").format("hh:mm A")
-console.log(rr);
-paraEl.textContent = dateEl;
-//paraE2.textContent = hourEl.format(" hh:mm");
+// var currentTime = moment();
+// console.log(currentTime);
+// currentTime = currentTime.startOf("day");
 
-paraE2.textContent = currentTime;
-$("#cal1").text(hourEl);
-$("#cal2").text(rr);
+// var hourEl = currentTime.add(9, 'h');
+// var rr = moment().hour("13").minutes("00").format("hh:mm A")
+// console.log(rr);
+// paraEl.textContent = dateEl;
+// //paraE2.textContent = hourEl.format(" hh:mm");
+
+// paraE2.textContent = currentTime;
+// $("#cal1").text(hourEl);
+// $("#cal2").text(rr);
 
 var timeArray = [9, 10, 11, 12, 13, 14, 15, 16, 17];
 
@@ -29,14 +23,44 @@ timeArray.forEach(function (time) {
     var newTimeArray = [];
     newTimeArray.push(moment().hour(time).minutes("00").format("hh:mm A"));
     console.log(newTimeArray);
+    //create an element to append all the elements
+    var containerEl = $("<div>", {
+        class: "input-group input-group-lg",
+    });
+
+    //create an element to store time
+    var spanEl = $("<span>", {
+        class: "input-group-text ml-5 time-block hour",
+    });
+    spanEl.text(newTimeArray);
+
+
+    //create an element to store text
+    var inputEl = $("<input>", {
+        class: "form - control form",
+        type: "textarea",
+    });
+
+
+    //create an element to save text
+    var buttonEl = $("<button>", {
+        class: "btn fa fa-1x fa-save btn-outline-secondary saveBtn",
+        type: "button",
+
+    });
+
+
+
+
+
 
     var divEl = $("<div>");
 
 
     console.log(divEl.text(time));
 
-
-    $(".container").append(divEl);
+    $("#time-block").append(containerEl);
+    containerEl.append(spanEl, inputEl, buttonEl);
 
 
 
