ead5ec241c021f92c4116b21a5980a877642f794
Pri_030 <priyankasingh30@gmail.com>
2020-05-03 14:31:35 -0400

    updated readme

--- README.md
+++ README.md
@@ -1,1 +1,44 @@
-# Work-Day-Scheduler
+
+# Project: ***Work-Day-Scheduler***
+Objective: Create a simple calendar application that allows the user to save events for each hour of the day. This app will run in the browser and feature dynamically updated HTML and CSS powered by jQuery.
+
+### This project has the following script features:
+* The dynamically created elements to display time blocks using HTML, CSS, and jQuery
+* This project utilizes the use of moment.js for time calculation and formatting conversion
+* If, if/else statement to compare time with the current time
+* An event listener for the save buttons to save to local storage
+
+
+### This project has the following features:
+* A header with local current time and day
+* Timeblocks for standard business hours 9: AM to 5:00 PM. Each time block has the following components.
+    * Time display block
+    * Text area to save user input
+    * Save button that saves user input to local storage
+* Timeblock is color-coded to indicate whether it is in the past, present, or future
+    * Gray is past the current hour and readonly
+    * Red is on the current hour and editable
+    * Green is before the current hour and editable
+* The task manager aspect saves entries to local storage and when the page is refreshed the saved events persist
+
+
+### This project features responsive design using a Bootstrap layout
+*Has responsive layout for:*
+* Small devices (landscape phones, 576px and up) 
+* Medium devices (tablets, 768px and up) 
+* Large devices (desktops, 992px and up) 
+
+
+### To Execute File:
+Open in browser
+
+## Developer ##
+
+**Priyanka Singh**
+
+
+### Below are the links of the deployed project: ###
+
+*https://github.com/singhpri30/Work-Day-Scheduler*
+
+*https://singhpri30.github.io/Work-Day-Scheduler/*
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,5 @@
 //get the current data and day
-var currentDate = moment().format("MMMM Do YYYY, dddd hh:mm A"); //moment() will give the current date and time, format the date using format()
+var currentDate = moment().format("dddd, MMMM Do YYYY, hh:mm A"); //moment() will give the current date and time, format the date using format()
 $("#currentDay").text(currentDate);
 
 // time Array to hold time values
@@ -7,9 +7,7 @@ var timeArray = [9, 10, 11, 12, 13, 14, 15, 16, 17];
 
 // loop to iterate over timeArray
 timeArray.forEach(function (time) {
-
-    // an empty array to hold formatted timeArray values
-    var newTimeArray = [];
+    var newTimeArray = [];// an empty array to hold formatted timeArray values
 
     //converting values of timeArray into proper format using moment.js methods
     newTimeArray.push(moment().hour(time).minutes("00").format("hh:mm A"));
@@ -23,6 +21,7 @@ timeArray.forEach(function (time) {
     //create a div element to store time
     var timeEl = $("<div>", {
         class: "input-group-text time-block hour", //adding multiple classes to div element
+
     });
 
     //create a span element to display time
@@ -30,28 +29,23 @@ timeArray.forEach(function (time) {
     spanEl.text(newTimeArray); // setting text to newTimeArray
     timeEl.append(spanEl); //appending span to div element
 
-
     //create a text-area element to store user input
     var inputEl = $("<textarea>", {
         class: "form-control description w-auto rounded input-area",// adding multiple classes to textarea
         type: "textarea",
-
+        id: time,
     });
     //create a button element to save text-area input
     var buttonEl = $("<button>", {
         class: "btn fa fa-1x fa-save btn-outline-secondary saveBtn btnEl", //adding multiple classes to button
         type: "button",
-
     });
-
     //appending all the elements to container div
     containerEl.append(timeEl, inputEl, buttonEl);
 
     //appending container div element to display on the page
     $("#time-block").append(containerEl);
-
 });
-
 
 //looping over each row to display different color code
 $(".row").each(function () {
@@ -60,41 +54,37 @@ $(".row").each(function () {
 
     if (currentTime > timeBlock) {
         $(this).find(".input-area").addClass("past"); //adding the class past to text-area if currentTime > timeBlock
-
+        $(this).find(".input-area").prop('readonly', true);// adding readonly property to texarea when time is in past
     }
     else if (currentTime < timeBlock) {
         $(this).find(".input-area").addClass("future"); //adding the class future to text-area if currentTime < timeBlock
-
+        $(this).find(".input-area").removeAttr('readonly');// removing readonly property from texarea when time is in future
     }
     else if (currentTime === timeBlock) {
         $(this).find(".input-area").addClass("present");//adding the class present to text-area if currentTime === timeBlock
-        $(".input-area").removeClass("future");
     }
+});
 
-})
+//get data from local storage
+for (var i = 0; i < timeArray.length; i++) {
 
-
-
-
-
-
-
-
+    $("#" + timeArray[i]).val(localStorage.getItem(timeArray[i]));
+}
 
 //set local storage on button click event
-$(".btnEl").on("click", function () {
-
+$(".btnEl").on("click", function (event) {
+    event.preventDefault();
 
     //get the value of textarea on abutton click
-    var textEl = $(this).siblings(".input-area");
+    var userInput = $(this).siblings(".input-area").val();
 
-
-    //get the data attribute val of textarea.
-    var storageKey = textEl.attr("data-time");
-    console.log(storageKey);
-
+    //get the value of data attribute from parent element
+    var storageKey = $(this).parent(".row").attr("data-time");
 
     //save the values in local storage as key value pair
-    localStorage.setItem(storageKey, textEl.val());
-
+    localStorage.setItem(storageKey, userInput);
 })
+
+
+
+
