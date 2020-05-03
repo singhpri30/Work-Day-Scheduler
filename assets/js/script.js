d80647ff69d365c64f5a8abb33a69abf1f76804a
Pri_030 <priyankasingh30@gmail.com>
2020-05-03 11:57:25 +1000

    updated script file

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,26 +1,23 @@
 //get the current data and day
-var currentTime = moment().format("MMMM Do YYYY, dddd hh:mm A"); //moment() will give the current date and time format the date using format()
-$("#currentDay").text(currentTime);
-
-
+var currentDate = moment().format("MMMM Do YYYY, dddd hh:mm A"); //moment() will give the current date and time, format the date using format()
+$("#currentDay").text(currentDate);
 
 // time Array to hold time values
 var timeArray = [9, 10, 11, 12, 13, 14, 15, 16, 17];
 
 // loop to iterate over timeArray
-timeArray.forEach(function (time, index) {
+timeArray.forEach(function (time) {
 
     // an empty array to hold formatted timeArray values
     var newTimeArray = [];
 
     //converting values of timeArray into proper format using moment.js methods
     newTimeArray.push(moment().hour(time).minutes("00").format("hh:mm A"));
-    //console.log(newTimeArray);
-
 
     //create a div element to append all other elements
     var containerEl = $("<div>", {
         class: "input-group input-group row", //adding multiple classes to div element
+        "data-time": time,
     });
 
     //create a div element to store time
@@ -38,10 +35,8 @@ timeArray.forEach(function (time, index) {
     var inputEl = $("<textarea>", {
         class: "form-control description w-auto rounded input-area",// adding multiple classes to textarea
         type: "textarea",
-        "data-time": index,
+
     });
-
-
     //create a button element to save text-area input
     var buttonEl = $("<button>", {
         class: "btn fa fa-1x fa-save btn-outline-secondary saveBtn btnEl", //adding multiple classes to button
@@ -49,49 +44,40 @@ timeArray.forEach(function (time, index) {
 
     });
 
-
     //appending all the elements to container div
     containerEl.append(timeEl, inputEl, buttonEl);
 
     //appending container div element to display on the page
     $("#time-block").append(containerEl);
 
-    //check condition for past, present and future times
-
-
-    // var compareTime = moment();
-    // console.log(compareTime);
-    //compareTime = compareTime.startOf("hour");
-
-
-    // if (compareTime.isAfter(timeArray)) {
-    //     console.log(compareTime + " " + newTimeArray);
-    //     $(".input-area").addClass("past");
-    // }
-    // else if (compareTime.isBefore(timeArray)) {
-    //     $(".form").addClass("future");
-    // }
-    // else if (compareTime.isSame(newTimeArray)) {
-    //     $(".form").addClass("present");
-    // };
-
 });
 
 
+//looping over each row to display different color code
+$(".row").each(function () {
+    var currentTime = moment().hours(); //storing current time
+    var timeBlock = parseInt($(this).attr("data-time")); //getting the data-time attribute value and storing it in a variable
+
+    if (currentTime > timeBlock) {
+        $(this).find(".input-area").addClass("past"); //adding the class past to text-area if currentTime > timeBlock
+
+    }
+    else if (currentTime < timeBlock) {
+        $(this).find(".input-area").addClass("future"); //adding the class future to text-area if currentTime < timeBlock
+
+    }
+    else if (currentTime === timeBlock) {
+        $(this).find(".input-area").addClass("present");//adding the class present to text-area if currentTime === timeBlock
+        $(".input-area").removeClass("future");
+    }
+
+})
 
 
-//get local storage data
 
-var storedVal = localStorage.getItem(storageKey);
-if (storedVal !== null) {
 
-    //     $(".input-area").val(storedVal);
-    // }
-    // else {
-    console.log(storedVal);
-    // }
 
-};
+
 
 
 
